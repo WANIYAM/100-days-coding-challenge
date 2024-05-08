55b9bad7fb3628a2a72f3fc074689d49342ca6e8
WANIYAM <waniya.mustafa75@gmail.com>
2024-05-08 16:29:40 +0500

    Added challenge questions for Day 18

---
+++ day-18/question-52.js
@@ -0,0 +1,15 @@
+/*                                         QUESTION 52
+Make a Smartphone Object: Create a simple way to keep track of a smartphone's details.
+Include its brand, model, and other key features like how much storage it has, the size of its screen,
+and how long its battery lasts.
+*/
+// Sets up details about a smartphone
+var Smartphone = {
+    brand: "Apple",
+    model: "iPhone 12",
+    storage: 128,
+    screenSize: 6.1,
+    batteryLife: 17
+};
+// Shows what we've set up about the smartphone
+console.log(Smartphone);
