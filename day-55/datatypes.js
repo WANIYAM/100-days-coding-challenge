773a111eb0f37e551e5e06b4a0a9be92ab4cc46e
WANIYAM <waniya.mustafa75@gmail.com>
2024-06-16 12:34:56 +0500

    Added challenge questions for Day 55

---
+++ day-55/datatypes.js
@@ -0,0 +1,12 @@
+let name = "Ilya";
+// Instead of alert, use console.log
+console.log(`hello ${1}`); 
+console.log(`hello ${"name"}`);
+console.log(`hello ${name}`);
+
+
+let Name = "Ilya";
+
+alert(`hello ${1}`);
+alert(`hello ${"Name"}`);
+alert(`hello ${Name}`);
