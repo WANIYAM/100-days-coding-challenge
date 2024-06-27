fbdba761fefb15fa37467f1c475d9caf24c01f67
WANIYAM <waniya.mustafa75@gmail.com>
2024-06-27 11:30:48 +0500

    Added challenge questions for Day 69

---
+++ day-69/enum.ts
@@ -0,0 +1,14 @@
+enum Days {
+    Sunday,
+    Monday,
+    Tuesday,
+    Wednesday,
+    Thursday,
+    Friday,
+    Saturday
+  }
+  
+  const today: Days = Days.Wednesday;
+  
+  console.log(`Today is ${Days[today]}`);
+  
